2ff9193f6315c442b3bb099fe533e0178d5c2a16
Alijan Rahimi <alijanrahimi24@yahoo.com>
2024-10-11 22:57:34 +0500

    add header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,12 @@ import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Project from "./pages/Project";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
