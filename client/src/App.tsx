f4a1014ddb84816ace8ea6843688d65e39bedac9
Alijan Rahimi <alijanrahimi24@yahoo.com>
2024-10-11 22:29:54 +0500

    setup the project routs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Project from "./pages/Project";
 
 function App() {
   return (
-    <div>Blog app</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Project />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
