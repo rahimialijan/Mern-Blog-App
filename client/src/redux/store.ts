43171a87fd37fcb83e158cd177e0725ba88954df
Alijan Rahimi <alijanrahimi24@yahoo.com>
2024-10-14 22:12:23 +0500

    add redux persist for project

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,14 +1,28 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import userReducer from './auth/userSlice';
+import storage from 'redux-persist/lib/storage';
+import {persistReducer} from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
 
+const rootReducer = combineReducers({
+    user:userReducer,
+});
 
- const store = configureStore({
-    reducer:{
-        user:userReducer,
-    }
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+ export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    })
 });
 
 
-export default store;
+export const  persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
