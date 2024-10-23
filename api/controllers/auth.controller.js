a6ef7a1bab25df13a307de3ad8862e63c84e2f00
Alijan Rahimi <alijanrahimi24@yahoo.com>
2024-10-23 20:53:05 +0500

    add OAoth google account functionality for project

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -65,3 +65,45 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
+
+
+
+export const google = async (req, res, next) => {
+  const { username, email, photo} = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+      const {password: pass, ...rest} = user._doc;
+      res
+        .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        }).json(rest);
+    } else {
+      const generatePassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+      const hashPassword = bcryptjs.hashSync(generatePassword, 10);
+      const newUser = new User({
+        username: username.toLowerCase().split(" ").join("") + Math.random().toString(9).slice(-4),
+        email,
+        password: hashPassword,
+        profilePicture: photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      })
+      const {password: pass, ...rest} = newUser._doc;
+      res
+        .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        }).json(rest);
+    }
+  
+} catch(error) {
+    next(error);
+  }
+}
--- client/src/utils/OAuth.tsx
+++ client/src/utils/OAuth.tsx
@@ -2,32 +2,49 @@ import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../../firebase";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../redux/auth/userSlice";
+import { useNavigate } from "react-router-dom";
+
+interface signUpResponseType {
+  message: string;
+  success: boolean;
+  user?: {
+    id: string;
+    username: string;
+    email: string;
+  };
+}
 
 function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const auth = getAuth(app);
   const handleWithGoogle = async () => {
-    const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
     provider.getCustomParameters();
     try {
       const resultsFromGoogle = await signInWithPopup(auth, provider);
+      console.log('resultsFromGoogle', resultsFromGoogle)
       const { displayName, email, photoURL } = resultsFromGoogle.user;
       const formData = {
         username: displayName,
         email: email,
         photo: photoURL,
       };
-      console.log(resultsFromGoogle);
-      const response = await axios.post("/api/auth/google", formData, {
+      const response: AxiosResponse<signUpResponseType> = await axios.post("/api/auth/google", formData, {
         headers: {
           "Content-Type": "application/json",
         },
       });
-      if (response.data>=200 && response.data<300) {
-        console.log("success");
-      } 
+      if (response.status >= 200 && response.status < 300) {
+        dispatch(loginSuccess(response.data));
+        console.log("response", response);
+        navigate('/')
+      }
     } catch (error) {
-      console.log(error);
+      console.log("error for catch block",error);
     }
   };
   return (
