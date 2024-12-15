8d6db17d06defa580db155b5c28eb01c8c971fc2
Alijan Rahimi <alijanrahimi24@yahoo.com>
2024-12-15 11:45:57 +0500

    complete the funtionality for update user profile

--- client/src/Components/DashProfile.tsx
+++ client/src/Components/DashProfile.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { app } from "../../firebase";
@@ -20,7 +20,8 @@ function DashProfile() {
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState<number | null>(null);
   const [imageFileUploadError, setImageFileUploadError] = useState<string | null>(null)
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
-
+  const [updateFailureMessage, setUpdateFailureMessage] = useState<string | null>(null)
+const [updateUserSucess, setUpdateUserSucess] = useState<string | null>(null)
   console.log('current user id',currentUser._id)
  const filePickerRef = useRef<HTMLInputElement>(null)
  const [formData, setFormData] = useState<FormDataProps>({
@@ -73,7 +74,16 @@ const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
   console.log(formData);
-  if (Object.keys(formData).length === 0) return;
+  if (
+    formData.username === currentUser.username &&
+    formData.email === currentUser.email &&
+    (!formData.password || formData.password === "") &&
+    formData.profilePicture === currentUser.profilePicture
+  ) {
+    setUpdateFailureMessage('No changes made');
+    setUpdateUserSucess(null)
+    return;
+  }
   try {
     dispatch(updateStart());
     const response = await fetch(`http://localhost:5173/api/user/update/${currentUser._id}`, {
@@ -89,13 +99,20 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
     if (response.ok) {
       dispatch(updateSuccess(data));
+      setUpdateUserSucess('User updated successfully')
+      setUpdateFailureMessage(null)
     } else {
-      dispatch(updateFailure(data));
+      dispatch(updateFailure(data.message));
+      setUpdateFailureMessage(data.message)
+      setUpdateUserSucess(null)
     }
   } catch (error) {
     dispatch(updateFailure(error as string));
+    setUpdateFailureMessage(error as string)
+    setUpdateUserSucess(null)
   }
 };
+
 
   return (
     <div className="max-auto p-3 w-full">
@@ -135,11 +152,14 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         <Button type="submit" gradientDuoTone="purpleToBlue" outline>
           Update
         </Button>
+      
       </form>
       <div className="flex mt-5 justify-between text-red-500 max-w-xl mx-auto">
         <span className="cursor-pointer">Delete Account</span>
         <span className="cursor-pointer">Sign Out</span>
       </div>
+      {updateUserSucess && <Alert color="success" className="text-green-500 text-center mt-5">{updateUserSucess}</Alert>}
+      {updateFailureMessage && <Alert color="failure" className="text-red-500 text-center mt-5">{updateFailureMessage}</Alert>}
     </div>
   );
 }
